6eb9d9e98a6246b8ad4dcb1539b72650a319cb0c
ngxson <thichthat@gmail.com>
2023-08-21 00:11:33 +0200

    fix https proxy

--- https_proxy/index.js
+++ https_proxy/index.js
@@ -1,15 +1,26 @@
-var httpProxy = require('http-proxy');
-var fs = require('fs');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 if (!process.env.TARGET) {
   console.error('TARGET env variable is not set');
   process.exit(1);
 }
 
-httpProxy.createProxyServer({
+const app = express();
+
+app.use(createProxyMiddleware({
   target: process.env.TARGET,
-  ssl: {
-    key: fs.readFileSync('key.pem', 'utf8'),
-    cert: fs.readFileSync('cert.pem', 'utf8')
-  }
-}).listen(443).on('error', console.error);
+  ws: true,
+}));
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer({
+  key: fs.readFileSync('key.pem', 'utf8'),
+  cert: fs.readFileSync('cert.pem', 'utf8')
+}, app);
+
+httpServer.listen(80);
+httpsServer.listen(443);
